4f3ac832755a16fffcdfe62689a80213400f8df7
Amankeldi Kairbay <32484171+amankhair@users.noreply.github.com>
2019-03-13 08:31:21 +0600

    Project added

---
+++ AddressBook/Person.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Person.aspx.cs" Inherits="AddressBook.Person" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div style="width=300">
+            <asp:HiddenField ID="hfId" runat="server" />
+            <table>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label1" runat="server" Text="Full Name"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtFullName" runat="server"></asp:TextBox>
+                    </td>
+               </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label2" runat="server" Text="Birth Date"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtBirthDate" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label3" runat="server" Text="Description"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txtDescription" runat="server" TextMode="MultiLine"></asp:TextBox>
+                    </td>
+                 </tr>
+                <tr>
+                    <td>
+                        <asp:Label ID="Label4" runat="server" Text="Email"></asp:Label>
+                    </td>
+                    <td colspan="2">
+                        <asp:TextBox ID="txteMAIL" runat="server"></asp:TextBox>
+                    </td>
+                 </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblSuccessMessage" runat="server" Text="Success" ForeColor="Green"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td colspan="2">
+                        <asp:Label ID="lblErrorMessage" runat="server" Text="Error" ForeColor="Red"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+            <br />
+                </div>
+            <div>
+            <asp:GridView ID="gvPerson" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="fullName" HeaderText="Full Name" />
+                    <asp:BoundField DataField="birthDate" HeaderText="Birth Date" />
+                    <asp:BoundField DataField="description" HeaderText="Description" />
+                    <asp:BoundField DataField="email" HeaderText="Email" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="lnkView" runat="server" CommandArgument='<%# Eval("Id") %>' OnClick="lnk_OnClick"> Edit </asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+                </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AddressBook/Person.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AddressBook
+{
+    public partial class Person : System.Web.UI.Page
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\AddressBook.mdf';Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                btnDelete.Enabled = false;
+                lblSuccessMessage.Text = lblErrorMessage.Text = "";
+                FillGridView();
+            }
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            hfId.Value = "";
+            txtBirthDate.Text = "";
+            txtFullName.Text = txtDescription.Text = txteMAIL.Text = "";
+            lblSuccessMessage.Text = lblErrorMessage.Text = "";
+            btnSave.Text = "Save";
+            btnDelete.Enabled = false;
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if(sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlCommand sqlCmd = new SqlCommand("PersonCreateOrUpdate", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@Id", (hfId.Value == ""?0:Convert.ToInt32(hfId.Value)));
+            sqlCmd.Parameters.AddWithValue("@fullName", txtFullName.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@birthDate", Convert.ToDateTime(txtBirthDate.Text));
+            sqlCmd.Parameters.AddWithValue("@description", txtDescription.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@email", txteMAIL.Text.Trim());
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            string personId = hfId.Value;
+            Clear();
+            if(personId == "")
+            {
+                lblSuccessMessage.Text = "Saved Successfully";
+            }
+            else
+            {
+                lblSuccessMessage.Text = "Updated Successfully";
+                FillGridView();
+            }
+        }
+
+        void FillGridView()
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlDataAdapter sqlData = new SqlDataAdapter("PersonViewAll", sqlCon);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            sqlCon.Close();
+            gvPerson.DataSource = dataTable;
+            gvPerson.DataBind();
+        }
+
+        protected void lnk_OnClick(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt32((sender as LinkButton).CommandArgument);
+            if(sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlDataAdapter sqlData = new SqlDataAdapter("PersonViewById", sqlCon);
+            sqlData.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sqlData.SelectCommand.Parameters.AddWithValue("@Id", Id);
+            DataTable dataTable = new DataTable();
+            sqlData.Fill(dataTable);
+            sqlCon.Close();
+            hfId.Value = Id.ToString();
+            txtFullName.Text = dataTable.Rows[0]["fullName"].ToString();
+            txtBirthDate.Text = dataTable.Rows[0]["birthDate"].ToString();
+            txtDescription.Text = dataTable.Rows[0]["description"].ToString();
+            txteMAIL.Text = dataTable.Rows[0]["email"].ToString();
+            btnSave.Text = "Update";
+            btnDelete.Enabled = true;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+            SqlCommand sqlCmd = new SqlCommand("PersonDeleteById", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@Id", Convert.ToInt32(hfId.Value));
+            sqlCmd.ExecuteNonQuery();
+            sqlCon.Close();
+            Clear();
+            FillGridView();
+            lblSuccessMessage.Text = "Deleted Successfully!";
+        }
+    }
+}
